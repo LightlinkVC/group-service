c5b96cc103363b775d550019cf93c5acaed83e05
artrsyf <rom.fadeev2017@yandex.ru>
2025-02-20 21:49:39 +0300

    Add SQL init script

---
+++ scripts/sql/init.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS roles (
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(255) NOT NULL UNIQUE 
+);
+
+CREATE TABLE IF NOT EXISTS group_types (
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS groups (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    creator_id INTEGER NOT NULL,
+    type_id INTEGER NOT NULL,
+    CONSTRAINT fk_group_type FOREIGN KEY (type_id) REFERENCES group_types(id)
+);
+
+CREATE TABLE IF NOT EXISTS group_members (
+    user_id INTEGER NOT NULL,
+    group_id INTEGER NOT NULL,
+    role_id INTEGER NOT NULL,
+    CONSTRAINT pk_group_member PRIMARY KEY (user_id, group_id),
+    CONSTRAINT fk_group FOREIGN KEY (group_id) REFERENCES groups(id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+INSERT INTO roles (name) VALUES
+    ('admin'),
+    ('member')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO group_types (name) VALUES
+    ('personal'),
+    ('group')
+ON CONFLICT (name) DO NOTHING;
