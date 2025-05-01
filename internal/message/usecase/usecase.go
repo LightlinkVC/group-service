2c3c1865b07b1c18f35c268d95a374a8770dec58
artrsyf <rom.fadeev2017@yandex.ru>
2025-05-01 15:29:18 +0300

    Implement notification logic

--- internal/message/usecase/usecase.go
+++ internal/message/usecase/usecase.go
@@ -1,27 +1,64 @@
 package usecase
 
 import (
-	"github.com/lightlink/group-service/internal/message/domain/dto"
+	"log"
+	"strconv"
+
+	groupRepo "github.com/lightlink/group-service/internal/group/repository"
+	messageDTO "github.com/lightlink/group-service/internal/message/domain/dto"
 	"github.com/lightlink/group-service/internal/message/domain/entity"
-	"github.com/lightlink/group-service/internal/message/repository"
+	messageRepo "github.com/lightlink/group-service/internal/message/repository"
+	notificationDTO "github.com/lightlink/group-service/internal/notification/domain/dto"
+	notificationRepo "github.com/lightlink/group-service/internal/notification/repository"
 )
 
 type MessageUsecaseI interface {
-	Create(createRequest *dto.CreateMessageRequest) (*entity.Message, error)
+	Create(createRequest *messageDTO.CreateMessageRequest) (*entity.Message, error)
 	GetByGroupID(groupID uint) ([]entity.Message, error)
 }
 
 type MessageUsecase struct {
-	messageRepo repository.MessageRepositoryI
+	messageRepo      messageRepo.MessageRepositoryI
+	groupRepo        groupRepo.GroupRepositoryI
+	notificationRepo notificationRepo.NotificationRepositoryI
 }
 
-func NewMessageUsecase(messageRepo repository.MessageRepositoryI) *MessageUsecase {
+func NewMessageUsecase(
+	messageRepo messageRepo.MessageRepositoryI,
+	notificationRepo notificationRepo.NotificationRepositoryI,
+	groupRepo groupRepo.GroupRepositoryI,
+) *MessageUsecase {
 	return &MessageUsecase{
-		messageRepo: messageRepo,
+		messageRepo:      messageRepo,
+		notificationRepo: notificationRepo,
+		groupRepo:        groupRepo,
 	}
 }
 
-func (uc *MessageUsecase) Create(createRequest *dto.CreateMessageRequest) (*entity.Message, error) {
+func (uc *MessageUsecase) sendIncomingMessageNotification(senderID, roomID uint, content string) {
+	receiverIDs, err := uc.groupRepo.GetMemberIDsByGroupID(roomID)
+	if err != nil {
+		log.Printf("ERR: An error occured due selecting group members: %v\n", err)
+	}
+
+	for _, receiverID := range receiverIDs {
+		if receiverID == senderID {
+			continue
+		}
+
+		uc.notificationRepo.Send(notificationDTO.RawNotification{
+			Type: "incomingMessage",
+			Payload: map[string]interface{}{
+				"from_user_id": strconv.FormatUint(uint64(senderID), 10),
+				"to_user_id":   strconv.FormatUint(uint64(receiverID), 10),
+				"room_id":      strconv.FormatUint(uint64(roomID), 10),
+				"content":      content,
+			},
+		})
+	}
+}
+
+func (uc *MessageUsecase) Create(createRequest *messageDTO.CreateMessageRequest) (*entity.Message, error) {
 	messageEntity := entity.Message{
 		UserID:  createRequest.UserID,
 		GroupID: createRequest.GroupID,
@@ -33,7 +70,13 @@ func (uc *MessageUsecase) Create(createRequest *dto.CreateMessageRequest) (*enti
 		return nil, err
 	}
 
-	createdMessageEntity := dto.MessageModelToEntity(createdMessageModel)
+	go uc.sendIncomingMessageNotification(
+		messageEntity.UserID,
+		messageEntity.GroupID,
+		messageEntity.Content,
+	)
+
+	createdMessageEntity := messageDTO.MessageModelToEntity(createdMessageModel)
 
 	return createdMessageEntity, nil
 }
